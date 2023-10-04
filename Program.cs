7366ecf15a1faae1fea7bfb71afe28ac573a100c
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2023-10-04 18:45:28 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Randy_AP1_P1;
+using Radzen;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+//Leer la ConnectionStrings llamada "Constr" que puse en el appsettings.json
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+//Inyectar el contexto para que este disponible
+//En los  constructores donde lo solicitemos
+builder.Services.AddDbContext<Contexto>(Op => Op.UseSqlite(ConStr));
+builder.Services.AddScoped<AportesBLL>();
+builder.Services.AddScoped<NotificationService>();
 
 var app = builder.Build();
 
